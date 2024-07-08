850c2012dd4210d0c5b37c7a746c199ba77d4ac1
glavo99 <elgendy.yousef.1999@gmail.com>
2024-07-08 16:32:59 +0300

    toLower

---
+++ APetyaAndStrings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    std::string a, b;
+    std::cin >> a >> b ;
+    std::transform (a.begin(), a.end(), a.begin(), tolower);
+    std::transform (b.begin(), b.end(), b.begin(), tolower);
+    if (a > b){
+        std::cout << 1 << std::endl;
+    }
+    else if (a < b){
+        std::cout << -1 << std::endl;
+    }
+    else{
+        std::cout << 0 << std::endl;
+    }
+    return 0;
+}
